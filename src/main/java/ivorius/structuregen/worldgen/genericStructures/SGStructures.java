f11be7afd21a75bab17b7edc868c696f2f1f20fa
Ivorius <lukastenbrink@googlemail.com>
2014-06-26 22:17:08 +0200

    Add forest maze to generating structures

--- src/main/java/ivorius/structuregen/worldgen/genericStructures/SGStructures.java
+++ src/main/java/ivorius/structuregen/worldgen/genericStructures/SGStructures.java
@@ -15,15 +15,17 @@ public class SGStructures
 {
     public static void registerModStructures()
     {
+        StructureHandler.registerStructures(StructureGen.MODID, false,
+                "StoneMaze3Way", "StoneMazeCorridor", "StoneMazeCrossing", "StoneMazeEnd", "StoneMazeEndChest", "StoneMazeLitRoom", "StoneMazeSpiderRoom", "StoneMazeTurn",
+                "ForestMaze3Way", "ForestMazeClearing", "ForestMazeCorridor", "ForestMazeCrossing", "ForestMazeEnd", "ForestMazeSlimes", "ForestMazeTurn"
+        );
         StructureHandler.registerStructures(StructureGen.MODID, true,
                 "MeteorSite",
                 "PirateChest", "OldWatchtower", "JokerTower", "DesertBeacon", "ForestBeacon", "DesertWatchtower", "PeacefulCrypt", "SmallAbandonedMine",
                 "SmallWoodenCottage", "SmallWoodenCottage1", "SmallWoodenCottage2", "DesertHut",
                 "ShrineSmallAir", "ShrineSmallEarth", "ShrineSmallFire", "ShrineSmallWater",
                 "MysticalTree", "MysticalTree1", "MysticalTree2",
-                "StoneMaze"
+                "StoneMaze", "ForestMaze"
         );
-        StructureHandler.registerStructures(StructureGen.MODID, false,
-                "StoneMaze3Way", "StoneMazeCorridor", "StoneMazeCrossing", "StoneMazeEnd", "StoneMazeEndChest", "StoneMazeLitRoom", "StoneMazeSpiderRoom", "StoneMazeTurn");
     }
 }

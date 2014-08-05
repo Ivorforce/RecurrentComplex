84c29aecea373a8981ef848ecd0ff48c833c0541
Ivorius <lukastenbrink@googlemail.com>
2014-08-05 23:40:00 +0100

    Rename Desert Fortress - Desert Fort

--- src/main/java/ivorius/reccomplex/worldgen/genericStructures/RCStructures.java
+++ src/main/java/ivorius/reccomplex/worldgen/genericStructures/RCStructures.java
@@ -21,7 +21,7 @@ public class RCStructures
         );
         StructureHandler.registerStructures(RecurrentComplex.MODID, generateDefaultStructures,
                 "MeteorSite",
-                "DesertFortress",
+                "DesertFort",
                 "PirateChest", "OldWatchtower", "JokerTower", "DesertBeacon", "ForestBeacon", "DesertWatchtower", "PeacefulCrypt", "SmallAbandonedMine",
                 "SmallWoodenCottage", "SmallWoodenCottage1", "SmallWoodenCottage2", "DesertHut",
                 "ShrineSmallAir", "ShrineSmallEarth", "ShrineSmallFire", "ShrineSmallWater",

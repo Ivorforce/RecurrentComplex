f4f0507897dbbcf35e1e156cbb392ad978debd67
Ivorius <lukastenbrink@googlemail.com>
2014-08-06 00:23:33 +0100

    Add Veldt Bath to generating structures

--- src/main/java/ivorius/reccomplex/worldgen/genericStructures/RCStructures.java
+++ src/main/java/ivorius/reccomplex/worldgen/genericStructures/RCStructures.java
@@ -21,7 +21,7 @@ public class RCStructures
         );
         StructureHandler.registerStructures(RecurrentComplex.MODID, generateDefaultStructures,
                 "MeteorSite",
-                "DesertFort",
+                "DesertFort", "VeldtBath",
                 "PirateChest", "OldWatchtower", "JokerTower", "DesertBeacon", "ForestBeacon", "DesertWatchtower", "PeacefulCrypt", "SmallAbandonedMine",
                 "SmallWoodenCottage", "SmallWoodenCottage1", "SmallWoodenCottage2", "DesertHut",
                 "ShrineSmallAir", "ShrineSmallEarth", "ShrineSmallFire", "ShrineSmallWater",

2d8271dd36fee9fdda91997445d3f305cffa3773
Ivorius <lukastenbrink@googlemail.com>
2014-08-08 12:35:09 +0100

    Set version to 0.9 - Beta

--- src/main/java/ivorius/reccomplex/RecurrentComplex.java
+++ src/main/java/ivorius/reccomplex/RecurrentComplex.java
@@ -49,7 +49,7 @@ public class RecurrentComplex
 {
     public static final String NAME = "Recurrent Complex";
     public static final String MODID = "reccomplex";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.9";
 
     public static final boolean USE_JSON_FOR_NBT = true;
     public static final boolean USE_ZIP_FOR_STRUCTURE_FILES = true;

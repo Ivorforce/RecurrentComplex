9599c2c325fd27f643d7eb773455139fb3b2ba85
Ivorius <lukastenbrink@googlemail.com>
2014-10-05 20:32:42 +0200

    Add two components to StoneMaze

--- src/main/java/ivorius/reccomplex/worldgen/genericStructures/RCStructures.java
+++ src/main/java/ivorius/reccomplex/worldgen/genericStructures/RCStructures.java
@@ -16,7 +16,7 @@ public class RCStructures
     public static void registerDefaultStructures(boolean generateDefaultStructures)
     {
         StructureHandler.registerStructures(RecurrentComplex.MODID, false,
-                "StoneMaze3Way", "StoneMazeCorridor", "StoneMazeCrossing", "StoneMazeEnd", "StoneMazeEndChest", "StoneMazeLitRoom", "StoneMazeSpiderRoom", "StoneMazeTurn",
+                "StoneMaze3Way", "StoneMazeCorridor", "StoneMazeCrossing", "StoneMazeEnd", "StoneMazeEndChest", "StoneMazeLitRoom", "StoneMazeSpiderRoom", "StoneMazeTurn", "StoneMazeFountain", "StoneMazeShootingRange",
                 "ForestMaze3Way", "ForestMazeClearing", "ForestMazeCorridor", "ForestMazeCrossing", "ForestMazeEnd", "ForestMazeSlimes", "ForestMazeTurn"
         );
         StructureHandler.registerStructures(RecurrentComplex.MODID, generateDefaultStructures,

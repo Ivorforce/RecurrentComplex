f81a774ff783b18a32d5acc9d32da1b7204bcb92
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2016-03-23 18:45:07 +0100

    Fix WorldScriptMulti reading and writing from NBT. Fixes #49.

--- src/main/java/ivorius/reccomplex/scripts/world/WorldScriptRegistry.java
+++ src/main/java/ivorius/reccomplex/scripts/world/WorldScriptRegistry.java
@@ -16,7 +16,7 @@ import java.util.Set;
  */
 public class WorldScriptRegistry extends NBTStringTypeRegistry<WorldScript>
 {
-    public static final WorldScriptRegistry INSTANCE = new WorldScriptRegistry("id", "script");
+    public static final WorldScriptRegistry INSTANCE = new WorldScriptRegistry("script", "id");
 
     public WorldScriptRegistry(String objectKey, String typeKey)
     {

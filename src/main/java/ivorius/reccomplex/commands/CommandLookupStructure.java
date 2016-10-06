1916c85e72a251ae6b9d17fba14cde7ebdc37a58
Lukas Tenbrink <lukastenbrink@googlemail.com>
2016-10-06 20:52:31 +0200

    Add current registration level to /#lookup.

--- src/main/java/ivorius/reccomplex/commands/CommandLookupStructure.java
+++ src/main/java/ivorius/reccomplex/commands/CommandLookupStructure.java
@@ -17,6 +17,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraft.util.text.event.HoverEvent;
 import org.apache.commons.lang3.StringUtils;
@@ -52,8 +53,8 @@ public class CommandLookupStructure extends CommandBase
     {
         if (args.length >= 1)
         {
-            String strucKey = args[0];
-            GenericStructureInfo structureInfo = CommandExportStructure.getGenericStructureInfo(strucKey);
+            String id = args[0];
+            GenericStructureInfo structureInfo = CommandExportStructure.getGenericStructureInfo(id);
             Metadata metadata = structureInfo.metadata;
 
             boolean hasAuthor = !metadata.authors.trim().isEmpty();
@@ -69,9 +70,12 @@ public class CommandLookupStructure extends CommandBase
                 weblink.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString(metadata.weblink)));
             }
 
+            ITextComponent level = new TextComponentString(StructureRegistry.INSTANCE.status(id).getLevel().toString());
+            level.getStyle().setColor(TextFormatting.YELLOW);
+
             commandSender.addChatMessage(ServerTranslations.format(
-                    StructureRegistry.INSTANCE.hasActive(strucKey) ? "commands.rclookup.reply.generates" : "commands.rclookup.reply.silent",
-                    strucKey, author, weblink));
+                    StructureRegistry.INSTANCE.hasActive(id) ? "commands.rclookup.reply.generates" : "commands.rclookup.reply.silent",
+                    id, author, level, weblink));
 
             if (!metadata.comment.trim().isEmpty())
                 commandSender.addChatMessage(ServerTranslations.format("commands.rclookup.reply.comment", metadata.comment));

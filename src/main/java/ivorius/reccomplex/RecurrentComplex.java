febd53179b6e6e048b28f0074674e2e422c61eeb
Lukas Tenbrink <lukastenbrink@googlemail.com>
2016-10-26 20:35:04 +0200

    Bump version to 0.9.7.2.

--- src/main/java/ivorius/reccomplex/RecurrentComplex.java
+++ src/main/java/ivorius/reccomplex/RecurrentComplex.java
@@ -44,7 +44,7 @@ public class RecurrentComplex
 {
     public static final String NAME = "Recurrent Complex";
     public static final String MODID = "reccomplex";
-    public static final String VERSION = "0.9.7.1";
+    public static final String VERSION = "0.9.7.2";
 
     public static final boolean USE_JSON_FOR_NBT = true;
     public static final boolean USE_ZIP_FOR_STRUCTURE_FILES = true;

e859394ff440514b9021dc8468a54f672fb94070
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-01-23 00:45:25 +0100

    Adjust the default ruins transformer values.

--- src/main/java/ivorius/reccomplex/world/gen/feature/structure/generic/transformers/TransformerRuins.java
+++ src/main/java/ivorius/reccomplex/world/gen/feature/structure/generic/transformers/TransformerRuins.java
@@ -82,7 +82,7 @@ public class TransformerRuins extends Transformer<TransformerRuins.InstanceData>
 
     public TransformerRuins()
     {
-        this(null, EnumFacing.DOWN, 0.0f, 0.9f, 0.3f, 1f / 25.0f,
+        this(null, EnumFacing.DOWN, 0.0f, 1f, 1f, 1f / 25.0f,
                 true, 0.3f, 0.1f);
     }
 

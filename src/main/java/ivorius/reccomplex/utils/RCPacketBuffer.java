d5e78d1bf5ce76c3159a34831051118e99857e9a
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-01-30 12:46:18 +0100

    Reverse supplier change and make more structures previable by using a larger size tracker.

---
+++ src/main/java/ivorius/reccomplex/utils/RCPacketBuffer.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (c) 2014, Lukas Tenbrink.
+ *  * http://ivorius.net
+ */
+
+package ivorius.reccomplex.utils;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
+import io.netty.handler.codec.EncoderException;
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.nbt.NBTSizeTracker;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
+
+import javax.annotation.Nullable;
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * Created by lukas on 30.01.17.
+ */
+public class RCPacketBuffer extends PacketBuffer
+{
+    public RCPacketBuffer(ByteBuf wrapped)
+    {
+        super(wrapped);
+    }
+
+    @Nullable
+    public NBTTagCompound readBigTag() throws IOException
+    {
+        int i = this.readerIndex();
+        byte b0 = this.readByte();
+
+        if (b0 == 0)
+        {
+            return null;
+        }
+        else
+        {
+            this.readerIndex(i);
+
+            try
+            {
+                return CompressedStreamTools.read(new ByteBufInputStream(this), new NBTSizeTracker(2097152L * 4));
+            }
+            catch (IOException ioexception)
+            {
+                throw new EncoderException(ioexception);
+            }
+        }
+    }
+}

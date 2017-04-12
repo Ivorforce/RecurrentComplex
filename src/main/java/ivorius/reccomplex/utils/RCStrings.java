8e8106bf627d89967e7ce023c6be885ef4f1a46d
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-04-12 17:39:52 +0200

    Improve formatted String abbreviation.

--- src/main/java/ivorius/reccomplex/utils/RCStrings.java
+++ src/main/java/ivorius/reccomplex/utils/RCStrings.java
@@ -6,7 +6,8 @@
 package ivorius.reccomplex.utils;
 
 import net.minecraft.util.text.TextFormatting;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nonnull;
 
@@ -15,28 +16,76 @@ import javax.annotation.Nonnull;
  */
 public class RCStrings
 {
-    @Nonnull
-    public static String abbreviateFormatted(String string, int offset, int maxWidth)
-    {
-        String abbreviate = StringUtils.abbreviate(string, offset, maxWidth);
-
-        if (offset > 0 && abbreviate.startsWith("...") && abbreviate.length() > 3 )
-        {
-            char first = abbreviate.charAt(3);
-            abbreviate = first == '§' ? abbreviate : ("..." + abbreviate.substring(4)); // Cut off one char to avoid destroying §
+    /**
+     * From StringUtils
+     */
+    private static MutablePair<Integer, Integer> abbreviate(final String str, int offset, final int maxWidth) {
+        if (str == null) {
+            return null;
         }
-
-        if (abbreviate.endsWith("..."))
-        {
-            char last = abbreviate.charAt(abbreviate.length() - 4);
-            abbreviate = abbreviate.substring(0, abbreviate.length() - (last == '§' ? 4 : 3)) + TextFormatting.RESET + "...";
+        if (maxWidth < 4) {
+            throw new IllegalArgumentException("Minimum abbreviation width is 4");
         }
-
-        return abbreviate;
+        if (str.length() <= maxWidth) {
+            return MutablePair.of(0, str.length());
+        }
+        if (offset > str.length()) {
+            offset = str.length();
+        }
+        if (str.length() - offset < maxWidth - 3) {
+            offset = str.length() - (maxWidth - 3);
+        }
+        if (offset <= 4) {
+            return MutablePair.of(0, maxWidth - 3);
+        }
+        if (maxWidth < 7) {
+            throw new IllegalArgumentException("Minimum abbreviation width with offset is 7");
+        }
+        if (offset + maxWidth - 3 < str.length()) {
+            Pair<Integer, Integer> abbreviated = abbreviate(str.substring(offset), 0, maxWidth - 3);
+            return MutablePair.of(offset, offset + abbreviated.getRight());
+        }
+        return MutablePair.of(str.length() - (maxWidth - 3), str.length());
     }
 
-    public static String abbreviateFormatted(String string, int maxWidth)
+    @Nonnull
+    public static String abbreviateFormatted(String str, int offset, int maxWidth)
     {
-        return abbreviateFormatted(string, 0, maxWidth);
+        MutablePair<Integer, Integer> abbreviate = abbreviate(str, offset, maxWidth);
+        String abbreviated = str.substring(abbreviate.getLeft(), abbreviate.getRight());
+
+        if (abbreviate.getLeft() > 0)
+        {
+            // Get the current format back in
+            String prevFormat = "";
+            if (str.length() > abbreviate.getLeft())
+                for (int i = abbreviate.getLeft() - 1; i >= 0; i--)
+                {
+                    char fst = str.charAt(i);
+                    char scd = str.charAt(i + 1);
+                    if (fst == '§' && scd != '§')
+                        prevFormat = fst + (scd + prevFormat);
+                }
+
+            if (str.charAt(abbreviate.getLeft() - 1) == '§')
+                abbreviated = abbreviated.substring(1); // Cut off format char
+
+            abbreviated = "..." + prevFormat + abbreviated;
+        }
+
+        if (abbreviate.getRight() < str.length())
+        {
+            if (str.charAt(abbreviate.getRight() - 1) == '§')
+                abbreviated = abbreviated.substring(0, abbreviated.length() - 1); // Cut off format char
+
+            abbreviated = abbreviated + TextFormatting.RESET + "...";
+        }
+
+        return abbreviated;
+    }
+
+    public static String abbreviateFormatted(String str, int maxWidth)
+    {
+        return abbreviateFormatted(str, 0, maxWidth);
     }
 }

b8bcc736d167325f64212fd6ddb70ed626f38b56
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-05-04 22:13:55 +0200

    Add another preview simplification layer for sending.

--- src/main/java/ivorius/reccomplex/operation/OperationRegistry.java
+++ src/main/java/ivorius/reccomplex/operation/OperationRegistry.java
@@ -7,25 +7,34 @@ package ivorius.reccomplex.operation;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import ivorius.ivtoolkit.blocks.IvBlockCollection;
 import ivorius.ivtoolkit.lang.IvClasses;
+import ivorius.ivtoolkit.tools.IvWorldData;
 import ivorius.ivtoolkit.tools.NBTCompoundObjects;
 import ivorius.reccomplex.RecurrentComplex;
 import ivorius.reccomplex.capability.RCEntityInfo;
 import ivorius.reccomplex.commands.RCCommands;
+import ivorius.reccomplex.utils.RCPacketBuffer;
 import ivorius.reccomplex.utils.ServerTranslations;
 import ivorius.reccomplex.world.gen.feature.structure.OperationClearArea;
 import ivorius.reccomplex.world.gen.feature.structure.OperationGenerateStructure;
+import ivorius.reccomplex.world.gen.feature.structure.generic.GenericStructure;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.PlayerNotFoundException;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraft.util.text.event.HoverEvent;
 import net.minecraft.world.WorldServer;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -99,11 +108,67 @@ public class OperationRegistry
     }
 
     @Nullable
-    public static NBTTagCompound dummyOperation(Operation danglingOperation)
+    public static NBTTagCompound dummyOperation(@Nonnull Operation operation, int level)
     {
-        if (danglingOperation instanceof OperationGenerateStructure)
-            return writeOperation(new OperationClearArea(((OperationGenerateStructure) danglingOperation).generationArea()));
-        else
-            return null;
+        if (level == 0)
+            return writeOperation(operation);
+
+        if (operation instanceof OperationGenerateStructure)
+        {
+            OperationGenerateStructure genStructure = (OperationGenerateStructure) operation;
+
+            if (level == 1 && genStructure.structure != null)
+            {
+                IvWorldData dummyWorldData = genStructure.structure.constructWorldData();
+                dummyWorldData.entities.clear();
+                dummyWorldData.tileEntities.clear();
+                for (BlockPos pos : dummyWorldData.blockCollection.area())
+                    dummyWorldData.blockCollection.setBlockState(pos, (dummyWorldData.blockCollection.getBlockState(pos).isNormalCube() ? Blocks.STONE : Blocks.AIR).getDefaultState());
+
+                GenericStructure dummyStructure = new GenericStructure();
+                dummyStructure.worldDataCompound = dummyWorldData.createTagCompound();
+                return writeOperation(new OperationGenerateStructure(dummyStructure, genStructure.generationInfoID, genStructure.transform, genStructure.lowerCoord, genStructure.generateAsSource));
+            }
+            else if (level == 2)
+                return writeOperation(new OperationClearArea(genStructure.generationArea()));
+        }
+
+        return null;
+    }
+
+    public static void writeBestPreview(ByteBuf buffer, Operation operation)
+    {
+        if (operation == null)
+        {
+            ByteBufUtils.writeTag(buffer, null);
+            return;
+        }
+
+        NBTTagCompound nbt;
+        int level = 0;
+        while ((nbt = dummyOperation(operation, level++)) != null)
+        {
+            if (canSend(nbt))
+            {
+                new RCPacketBuffer(buffer).writeNBTTagCompoundToBuffer(nbt);
+                return;
+            }
+        }
+
+        new RCPacketBuffer(buffer).writeNBTTagCompoundToBuffer(null);
+    }
+
+    protected static boolean canSend(NBTTagCompound danglingNBT)
+    {
+        ByteBuf temp = Unpooled.buffer();
+        new RCPacketBuffer(temp).writeNBTTagCompoundToBuffer(danglingNBT);
+
+        return canSend(temp);
+    }
+
+    protected static boolean canSend(ByteBuf temp)
+    {
+        // From SPacketCustomPayload
+        return temp.writerIndex() <= (1048576 * 4 / 5);
     }
 }

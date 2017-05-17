f5581c07a9e6a866d83f03a4e3edf0381b20b32c
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-05-17 13:24:52 +0200

    Intersect the generation bb before running through the blocks to save on performance on large partial structures.

---
+++ src/main/java/ivorius/reccomplex/utils/RCAxisAlignedTransform.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (c) 2014, Lukas Tenbrink.
+ *  * http://ivorius.net
+ */
+
+package ivorius.reccomplex.utils;
+
+import ivorius.ivtoolkit.blocks.BlockArea;
+import ivorius.ivtoolkit.blocks.BlockPositions;
+import ivorius.ivtoolkit.math.AxisAlignedTransform2D;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+
+/**
+ * Created by lukas on 22.09.16.
+ */
+public class RCAxisAlignedTransform
+{
+    public static BlockPos.MutableBlockPos apply(BlockPos.MutableBlockPos position, int[] size, AxisAlignedTransform2D transform)
+    {
+        return apply(position, position, size, transform);
+    }
+
+    public static BlockPos.MutableBlockPos apply(BlockPos position, BlockPos.MutableBlockPos onPosition, int[] size, AxisAlignedTransform2D transform)
+    {
+        boolean mirrorX = transform.isMirrorX();
+        int rotation = transform.getRotation();
+
+        int positionX = mirrorX ? size[0] - 1 - position.getX() : position.getX();
+
+        switch (rotation)
+        {
+            case 0:
+                return onPosition.setPos(positionX, position.getY(), position.getZ());
+            case 1:
+                return onPosition.setPos(size[2] - 1 - position.getZ(), position.getY(), positionX);
+            case 2:
+                return onPosition.setPos(size[0] - 1 - positionX, position.getY(), size[2] - 1 - position.getZ());
+            case 3:
+                return onPosition.setPos(position.getZ(), position.getY(), size[0] - 1 - positionX);
+            default:
+                throw new InternalError();
+        }
+    }
+
+    public static BlockArea apply(BlockArea area, int[] size, AxisAlignedTransform2D transform)
+    {
+        return apply(area, new BlockArea(BlockPos.ORIGIN, BlockPos.ORIGIN), size, transform);
+    }
+
+    public static BlockArea apply(BlockArea area, BlockArea on, int[] size, AxisAlignedTransform2D transform)
+    {
+        on.setPoint1(transform.apply(area.getPoint1(), size));
+        on.setPoint2(transform.apply(area.getPoint2(), size));
+        return on;
+    }
+
+    public static AxisAlignedTransform2D invert(AxisAlignedTransform2D transform2D)
+    {
+        // Black Magic
+        return AxisAlignedTransform2D.from(transform2D.isMirrorX() ? transform2D.getRotation() : -transform2D.getRotation(),
+                transform2D.isMirrorX());
+    }
+
+}
---
+++ src/main/java/ivorius/reccomplex/utils/RCBlockAreas.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2014, Lukas Tenbrink.
+ *  * http://ivorius.net
+ */
+
+package ivorius.reccomplex.utils;
+
+import ivorius.ivtoolkit.blocks.BlockArea;
+import ivorius.ivtoolkit.blocks.BlockPositions;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+
+/**
+ * Created by lukas on 17.05.17.
+ */
+public class RCBlockAreas
+{
+    public static BlockArea add(BlockArea area, Vec3i pos)
+    {
+        area.setPoint1(area.getPoint1().add(pos));
+        area.setPoint2(area.getPoint2().add(pos));
+        return area;
+    }
+
+    public static BlockArea sub(BlockArea area, Vec3i pos)
+    {
+        area.setPoint1(area.getPoint1().subtract(pos));
+        area.setPoint2(area.getPoint2().subtract(pos));
+        return area;
+    }
+
+    public static BlockArea intersect(BlockArea left, BlockArea right)
+    {
+        return new BlockArea(BlockPositions.getHigherCorner(left.getLowerCorner(), right.getLowerCorner()),
+                BlockPositions.getLowerCorner(left.getHigherCorner(), right.getHigherCorner()));
+    }
+
+    public static BlockArea from(StructureBoundingBox bb)
+    {
+        return new BlockArea(new BlockPos(bb.minX, bb.minY, bb.minZ), new BlockPos(bb.maxX, bb.maxY, bb.maxZ));
+    }
+}

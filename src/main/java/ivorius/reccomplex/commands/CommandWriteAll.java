bc6a84db091c60db00ab6b03ad7fbe7500b8dd8b
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-05-31 18:26:48 +0200

    Fix writeAll compile error.

--- src/main/java/ivorius/reccomplex/commands/CommandWriteAll.java
+++ src/main/java/ivorius/reccomplex/commands/CommandWriteAll.java
@@ -96,7 +96,7 @@ public class CommandWriteAll extends CommandBase
         // Can't chain because of compiler bug :|
 
         expect.next(RecurrentComplex.saver.keySet());
-        expect.next(args1 -> parameters.get().first().optional().map(RecurrentComplex.saver::get).map(a -> a.getRegistry().ids()).orElse(Collections.emptySet()));
+        expect.next(args1 -> parameters.get().first().tryGet().map(RecurrentComplex.saver::get).map(a -> a.getRegistry().ids()).orElse(Collections.emptySet()));
         expect.named("dir").resourceDirectory();
 
         return expect.get(server, sender, args, pos);

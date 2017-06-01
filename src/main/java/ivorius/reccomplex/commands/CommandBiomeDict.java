c43803828adaed3177dcde64ba2e56d28b3570c0
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-06-01 14:47:52 +0200

    Add CommandSplit and SimpleCommand to make split commands more usable.

--- src/main/java/ivorius/reccomplex/commands/CommandBiomeDict.java
+++ src/main/java/ivorius/reccomplex/commands/CommandBiomeDict.java
@@ -8,21 +8,17 @@ package ivorius.reccomplex.commands;
 import com.google.common.collect.Lists;
 import ivorius.ivtoolkit.tools.IvGsonHelper;
 import ivorius.reccomplex.RCConfig;
-import ivorius.reccomplex.commands.parameters.RCExpect;
-import ivorius.reccomplex.commands.parameters.RCParameters;
+import ivorius.reccomplex.commands.parameters.*;
 import ivorius.reccomplex.utils.ServerTranslations;
 import ivorius.reccomplex.utils.accessor.RCAccessorBiomeDictionary;
-import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -30,8 +26,61 @@ import java.util.List;
 /**
  * Created by lukas on 03.08.14.
  */
-public class CommandBiomeDict extends CommandBase
+public class CommandBiomeDict extends CommandSplit
 {
+    public CommandBiomeDict()
+    {
+        add(new SimpleCommand("search", "<terms>", Expect::start)
+        {
+            @Override
+            public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+            {
+                RCParameters parameters = RCParameters.of(args);
+
+                List<String> terms = parameters.get().varargsList();
+
+                CommandSearchStructure.postResultMessage(sender,
+                        RCTextStyle::biome,
+                        CommandSearchStructure.search(Biome.REGISTRY.getKeys(), loc -> CommandSearchStructure.searchRank(terms, keywords(loc, Biome.REGISTRY.getObject(loc))))
+                );
+            }
+        });
+
+        add(new SimpleCommand("types", "<biome>", () -> RCExpect.startRC().biome())
+        {
+            @Override
+            public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+            {
+                RCParameters parameters = RCParameters.of(args);
+
+                String biomeID = parameters.get().first().require();
+                Biome biome = parameters.mc().biome().require();
+
+                sender.sendMessage(ServerTranslations.format("commands.biomedict.get", biomeID,
+                        ServerTranslations.join(Lists.newArrayList(BiomeDictionary.getTypes(biome)).stream()
+                                .map(RCTextStyle::biomeType).toArray())
+                ));
+            }
+        });
+
+        add(new SimpleCommand("list", "<biome type", () -> RCExpect.startRC().next(RCAccessorBiomeDictionary.getMap().keySet()))
+        {
+            @Override
+            public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+            {
+                RCParameters parameters = RCParameters.of(args);
+
+                String typeID = parameters.get().first().require();
+                BiomeDictionary.Type type = parameters.mc().biomeDictionaryType().require();
+
+                sender.sendMessage(ServerTranslations.format("commands.biomedict.list", typeID,
+                        ServerTranslations.join(Lists.newArrayList(BiomeDictionary.getBiomes(type))
+                                .stream().map(RCTextStyle::biome).toArray())
+                ));
+            }
+        });
+    }
+
     @Nonnull
     public static Collection<String> keywords(ResourceLocation id, Biome biome)
     {
@@ -39,7 +88,7 @@ public class CommandBiomeDict extends CommandBase
     }
 
     @Override
-    public String getCommandName()
+    public String getName()
     {
         return RCConfig.commandPrefix + "biome";
     }
@@ -47,77 +96,5 @@ public class CommandBiomeDict extends CommandBase
     public int getRequiredPermissionLevel()
     {
         return 2;
-    }
-
-    @Override
-    public String getCommandUsage(ICommandSender commandSender)
-    {
-        return ServerTranslations.usage("commands.biomedict.usage");
-    }
-
-    @Override
-    public void execute(MinecraftServer server, ICommandSender commandSender, String[] args) throws CommandException
-    {
-        RCParameters parameters = RCParameters.of(args);
-
-        switch (parameters.get().first().require())
-        {
-            case "search":
-            {
-                List<String> terms = parameters.get().move(1).varargsList();
-
-                CommandSearchStructure.postResultMessage(commandSender,
-                        RCTextStyle::biome,
-                        CommandSearchStructure.search(Biome.REGISTRY.getKeys(), loc -> CommandSearchStructure.searchRank(terms, keywords(loc, Biome.REGISTRY.getObject(loc))))
-                );
-                break;
-            }
-            case "types":
-            {
-                String biomeID = parameters.get().at(1).require();
-                Biome biome = parameters.mc().biome().require();
-
-                commandSender.sendMessage(ServerTranslations.format("commands.biomedict.get", biomeID,
-                        ServerTranslations.join(Lists.newArrayList(BiomeDictionary.getTypes(biome)).stream()
-                                .map(RCTextStyle::biomeType).toArray())
-                ));
-
-                break;
-            }
-            case "list":
-            {
-                BiomeDictionary.Type type = parameters.mc().move(1).biomeDictionaryType().require();
-
-                commandSender.sendMessage(ServerTranslations.format("commands.biomedict.list", args[1],
-                        ServerTranslations.join(Lists.newArrayList(BiomeDictionary.getBiomes(type))
-                                .stream().map(RCTextStyle::biome).toArray())
-                ));
-                break;
-            }
-            default:
-                throw ServerTranslations.wrongUsageException("commands.biomedict.usage");
-        }
-    }
-
-    @Override
-    public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos)
-    {
-        RCExpect<?> expect = RCExpect.startRC()
-                .any("types", "list", "search");
-
-        switch (args[0])
-        {
-            case "types":
-                expect.biome();
-                break;
-            case "list":
-                expect.next(RCAccessorBiomeDictionary.getMap().keySet());
-                break;
-            default:
-                expect.skip(1);
-                break;
-        }
-
-        return expect.get(server, sender, args, pos);
     }
 }
---
+++ src/main/java/ivorius/reccomplex/commands/parameters/SimpleCommand.java
@@ -0,0 +1,56 @@
+/*
+ *  Copyright (c) 2014, Lukas Tenbrink.
+ *  * http://ivorius.net
+ */
+
+package ivorius.reccomplex.commands.parameters;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * Created by lukas on 01.06.17.
+ */
+public abstract class SimpleCommand extends CommandBase
+{
+    public String name;
+
+    public String usage;
+    public Supplier<Expect<?>> expector;
+
+    public SimpleCommand(String name)
+    {
+        this.name = name;
+    }
+
+    public SimpleCommand(String name, String usage, Supplier<Expect<?>> expector)
+    {
+        this.name = name;
+        this.usage = usage;
+        this.expector = expector;
+    }
+
+    @Override
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender)
+    {
+        return String.format("/%s %s", name, usage);
+    }
+
+    @Override
+    public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos)
+    {
+        return expector.get().get(server, sender, args, targetPos);
+    }
+}

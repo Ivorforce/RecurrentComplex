26c58215949ac5da1cadb3930eee6451579c1bfe
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-06-01 15:36:22 +0200

    Change /#select into a split command.

--- src/main/java/ivorius/reccomplex/commands/CommandSelect.java
+++ src/main/java/ivorius/reccomplex/commands/CommandSelect.java
@@ -8,25 +8,70 @@ package ivorius.reccomplex.commands;
 import ivorius.ivtoolkit.math.IvVecMathHelper;
 import ivorius.reccomplex.RCConfig;
 import ivorius.reccomplex.capability.SelectionOwner;
-import ivorius.reccomplex.commands.parameters.RCExpect;
-import ivorius.reccomplex.commands.parameters.RCParameters;
+import ivorius.reccomplex.commands.parameters.*;
 import ivorius.reccomplex.utils.ServerTranslations;
-import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 
-import javax.annotation.Nullable;
-import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * Created by lukas on 25.05.14.
  */
-public class CommandSelect extends CommandBase
+public class CommandSelect extends CommandSplit
 {
+    public CommandSelect()
+    {
+        add(new Command("clear")
+        {
+            @Override
+            public void execute(MinecraftServer server, ICommandSender sender, RCParameters parameters, SelectionOwner owner) throws CommandException
+            {
+                owner.setSelection(null);
+            }
+        });
+
+        add(new Command("get")
+        {
+            @Override
+            public void execute(MinecraftServer server, ICommandSender sender, RCParameters parameters, SelectionOwner owner) throws CommandException
+            {
+                sender.sendMessage(ServerTranslations.format("commands.selectSet.get", translatePoint(owner.getSelectedPoint1()), translatePoint(owner.getSelectedPoint2())));
+                if (owner.hasValidSelection())
+                    sender.sendMessage(ServerTranslations.format("commands.selectSet.size", translateSize(owner.getSelection().areaSize()), IvVecMathHelper.product(owner.getSelection().areaSize())));
+            }
+        });
+
+        add(new Command("set", "[x] [y] [z] --first --second", () -> RCExpect.startRC().xyz().flag("first").flag("second"))
+        {
+            @Override
+            public void execute(MinecraftServer server, ICommandSender sender, RCParameters parameters, SelectionOwner owner) throws CommandException
+            {
+                boolean first = !parameters.has("second");
+                boolean second = !parameters.has("first");
+
+                if (first)
+                {
+                    if (owner.getSelectedPoint1() == null)
+                        owner.setSelectedPoint1(sender.getPosition());
+
+                    owner.setSelectedPoint1(parameters.mc().pos(owner.getSelectedPoint1(), false).require());
+                }
+                if (second)
+                {
+                    if (owner.getSelectedPoint2() == null)
+                        owner.setSelectedPoint2(sender.getPosition());
+
+                    owner.setSelectedPoint2(parameters.mc().pos(owner.getSelectedPoint2(), false).require());
+                }
+            }
+        });
+    }
+
     @Override
-    public String getCommandName()
+    public String getName()
     {
         return RCConfig.commandPrefix + "select";
     }
@@ -34,54 +79,6 @@ public class CommandSelect extends CommandBase
     public int getRequiredPermissionLevel()
     {
         return 2;
-    }
-
-    @Override
-    public String getCommandUsage(ICommandSender var1)
-    {
-        return ServerTranslations.usage("commands.selectSet.usage");
-    }
-
-    @Override
-    public void execute(MinecraftServer server, ICommandSender commandSender, String[] args) throws CommandException
-    {
-        RCParameters parameters = RCParameters.of(args);
-
-        SelectionOwner owner = RCCommands.getSelectionOwner(commandSender, null, false);
-
-        String subcommand = parameters.get().first().require();
-        switch (subcommand)
-        {
-            case "clear":
-                owner.setSelection(null);
-                break;
-            case "get":
-                commandSender.sendMessage(ServerTranslations.format("commands.selectSet.get", translatePoint(owner.getSelectedPoint1()), translatePoint(owner.getSelectedPoint2())));
-                if (owner.hasValidSelection())
-                    commandSender.sendMessage(ServerTranslations.format("commands.selectSet.size", translateSize(owner.getSelection().areaSize()), IvVecMathHelper.product(owner.getSelection().areaSize())));
-                break;
-            case "both":
-            case "point1":
-            case "point2":
-                if (!subcommand.equals("point2"))
-                {
-                    if (owner.getSelectedPoint1() == null)
-                        owner.setSelectedPoint1(commandSender.getPosition());
-
-                    owner.setSelectedPoint1(parameters.mc().move(1).pos(owner.getSelectedPoint1(), false).require());
-                }
-                if (!subcommand.equals("point1"))
-                {
-                    if (owner.getSelectedPoint2() == null)
-                        owner.setSelectedPoint2(commandSender.getPosition());
-
-                    owner.setSelectedPoint2(parameters.mc().move(1).pos(owner.getSelectedPoint2(), false).require());
-                }
-
-                break;
-            default:
-                throw ServerTranslations.wrongUsageException("commands.selectSet.usage");
-        }
     }
 
     protected Object translatePoint(BlockPos coord)
@@ -98,12 +95,26 @@ public class CommandSelect extends CommandBase
                 : ServerTranslations.format("commands.selectSet.point.none");
     }
 
-    @Override
-    public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos)
+    public static abstract class Command extends SimpleCommand
     {
-        return RCExpect.startRC()
-                .any("both", "clear", "point1", "point2", "get")
-                .xyz()
-                .get(server, sender, args, pos);
+        public Command(String name)
+        {
+            super(name);
+        }
+
+        public Command(String name, String usage, Supplier<Expect<?>> expector)
+        {
+            super(name, usage, expector);
+        }
+
+        @Override
+        public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+        {
+            SelectionOwner owner = RCCommands.getSelectionOwner(sender, null, false);
+            RCParameters parameters = RCParameters.of(args);
+            execute(server, sender, parameters, owner);
+        }
+
+        public abstract void execute(MinecraftServer server, ICommandSender sender, RCParameters parameters, SelectionOwner owner) throws CommandException;
     }
 }

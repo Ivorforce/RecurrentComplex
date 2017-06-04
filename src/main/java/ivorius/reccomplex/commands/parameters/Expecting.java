ec3817e4dea74418cecb53e18bcb88efa62be828
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-06-04 04:03:41 +0200

    Add Expecting which makes usage generation a bit simpler. Fix the command name being showed twice.

---
+++ src/main/java/ivorius/reccomplex/commands/parameters/Expecting.java
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2014, Lukas Tenbrink.
+ *  * http://ivorius.net
+ */
+
+package ivorius.reccomplex.commands.parameters;
+
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+
+/**
+ * Created by lukas on 04.06.17.
+ */
+public interface Expecting extends ICommand
+{
+    Expect<?> expect();
+
+    @Nonnull
+    default String getUsage(ICommandSender var1)
+    {
+        return String.format("%s %s", getName(), expect().usage());
+    }
+
+    @Nonnull
+    default List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos)
+    {
+        return expect().get(server, sender, args, pos);
+    }
+}

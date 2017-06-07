fb020779cac0803b3903962aed6c9ce2836dd6eb
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-06-07 14:13:46 +0200

    Move map() to CommandMapStructure.

--- src/main/java/ivorius/reccomplex/commands/structure/CommandMapStructure.java
+++ src/main/java/ivorius/reccomplex/commands/structure/CommandMapStructure.java
@@ -10,21 +10,63 @@ import ivorius.ivtoolkit.world.MockWorld;
 import ivorius.reccomplex.RCConfig;
 import ivorius.reccomplex.commands.CommandSelecting;
 import ivorius.reccomplex.commands.CommandVirtual;
-import ivorius.reccomplex.commands.parameters.*;
+import ivorius.reccomplex.commands.parameters.CommandExpecting;
+import ivorius.reccomplex.commands.parameters.Expect;
+import ivorius.reccomplex.commands.parameters.RCExpect;
+import ivorius.reccomplex.commands.parameters.RCParameters;
 import ivorius.reccomplex.files.loading.ResourceDirectory;
 import ivorius.reccomplex.network.PacketSaveStructureHandler;
 import ivorius.reccomplex.utils.ServerTranslations;
+import ivorius.reccomplex.world.gen.feature.structure.Structure;
+import ivorius.reccomplex.world.gen.feature.structure.StructureRegistry;
 import ivorius.reccomplex.world.gen.feature.structure.generic.GenericStructure;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.math.BlockPos;
 
+import javax.annotation.Nonnull;
+
 /**
  * Created by lukas on 03.08.14.
  */
 public class CommandMapStructure extends CommandExpecting
 {
+    @Nonnull
+    public static MapResult map(String structureID, ResourceDirectory directory, ICommandSender commandSender, CommandVirtual command, String[] args, boolean inform) throws CommandException
+    {
+        Structure<?> info = StructureRegistry.INSTANCE.get(structureID);
+
+        if (!(info instanceof GenericStructure))
+        {
+            if (inform)
+                throw ServerTranslations.commandException("commands.structure.notGeneric", structureID);
+
+            return MapResult.SKIPPED;
+        }
+
+        GenericStructure structure = (GenericStructure) info;
+
+        IvWorldData worldData = structure.constructWorldData();
+        MockWorld world = new MockWorld.WorldData(worldData);
+
+        try
+        {
+            command.execute(world, new CommandSelecting.SelectingSender(commandSender, BlockPos.ORIGIN, worldData.blockCollection.area().getHigherCorner()),
+                    args);
+        }
+        catch (MockWorld.VirtualWorldException ex)
+        {
+            throw ServerTranslations.commandException("commands.rcmap.nonvirtual.arguments");
+        }
+
+        structure.worldDataCompound = worldData.createTagCompound();
+
+        return PacketSaveStructureHandler.write(commandSender, structure, structureID, directory, true, inform)
+                ? MapResult.SUCCESS
+                : MapResult.FAILED;
+    }
+
     @Override
     public String getName()
     {
@@ -56,6 +98,11 @@ public class CommandMapStructure extends CommandExpecting
         CommandVirtual virtual = parameters.get(1).virtualCommand(server).require();
         String[] virtualArgs = parameters.get(2).varargs();
 
-        CommandMapAllStructure.map(id, directory, sender, virtual, virtualArgs, true);
+        map(id, directory, sender, virtual, virtualArgs, true);
+    }
+
+    public enum MapResult
+    {
+        SUCCESS, FAILED, SKIPPED
     }
 }

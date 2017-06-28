ba959935ff65a8c65c85b34e076b8c28985e7eac
Lukas Tenbrink <lukastenbrink@googlemail.com>
2017-06-29 00:29:17 +0200

    Abstract runShape a bit from /#fill.

--- src/main/java/ivorius/reccomplex/commands/former/CommandSelectFill.java
+++ src/main/java/ivorius/reccomplex/commands/former/CommandSelectFill.java
@@ -9,24 +9,25 @@ import ivorius.ivtoolkit.blocks.BlockArea;
 import ivorius.ivtoolkit.blocks.BlockStates;
 import ivorius.ivtoolkit.math.IvShapeHelper;
 import ivorius.ivtoolkit.world.MockWorld;
+import ivorius.mcopts.commands.CommandExpecting;
+import ivorius.mcopts.commands.parameters.MCP;
+import ivorius.mcopts.commands.parameters.Parameters;
+import ivorius.mcopts.commands.parameters.expect.Expect;
+import ivorius.mcopts.commands.parameters.expect.MCE;
 import ivorius.reccomplex.RCConfig;
 import ivorius.reccomplex.capability.SelectionOwner;
 import ivorius.reccomplex.commands.CommandVirtual;
 import ivorius.reccomplex.commands.RCCommands;
-import ivorius.mcopts.commands.CommandExpecting;
-import ivorius.mcopts.commands.parameters.*;
-import ivorius.mcopts.commands.parameters.expect.Expect;
-import ivorius.mcopts.commands.parameters.expect.MCE;
-import ivorius.reccomplex.commands.parameters.expect.RCE;
 import ivorius.reccomplex.commands.parameters.RCP;
+import ivorius.reccomplex.commands.parameters.expect.RCE;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.command.WrongUsageException;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -35,6 +36,44 @@ import java.util.stream.IntStream;
  */
 public class CommandSelectFill extends CommandExpecting implements CommandVirtual
 {
+    public static void runShape(String shape, BlockArea area, Consumer<BlockPos> consumer) throws CommandException
+    {
+        BlockPos p1 = area.getPoint1();
+        BlockPos p2 = area.getPoint2();
+
+        switch (shape)
+        {
+            case "cube":
+                for (BlockPos pos : area)
+                    consumer.accept(pos);
+                break;
+            case "sphere":
+            {
+                double[] spheroidOrigin = new double[]{(p1.getX() + p2.getX()) * 0.5, (p1.getY() + p2.getY()) * 0.5, (p1.getZ() + p2.getZ()) * 0.5};
+                int[] areaSize = area.areaSize();
+                double[] spheroidSize = new double[]{areaSize[0] * 0.5, areaSize[1] * 0.5, areaSize[2] * 0.5};
+
+                for (BlockPos pos : area)
+                {
+                    double[] coordPoint = new double[]{pos.getX(), pos.getY(), pos.getZ()};
+                    if (IvShapeHelper.isPointInSpheroid(coordPoint, spheroidOrigin, spheroidSize))
+                        consumer.accept(pos);
+                }
+                break;
+            }
+            default:
+                throw new CommandException("Unknown Shape!");
+        }
+    }
+
+    public static void runShape(ICommandSender sender, String shape, Consumer<BlockPos> consumer) throws CommandException
+    {
+        SelectionOwner selectionOwner = RCCommands.getSelectionOwner(sender, null, true);
+        RCCommands.assertSize(sender, selectionOwner);
+
+        runShape(shape, selectionOwner.getSelection(), consumer);
+    }
+
     @Override
     public String getName()
     {
@@ -61,45 +100,12 @@ public class CommandSelectFill extends CommandExpecting implements CommandVirtua
         Block dstBlock = parameters.get(0).to(MCP.block(sender)).require();
         int[] dstMeta = parameters.get(1).to(RCP::metadatas).optional().orElse(new int[1]);
         List<IBlockState> dst = IntStream.of(dstMeta).mapToObj(m -> BlockStates.fromMetadata(dstBlock, m)).collect(Collectors.toList());
-
-        SelectionOwner selectionOwner = RCCommands.getSelectionOwner(sender, null, true);
-        RCCommands.assertSize(sender, selectionOwner);
-
         String shape = parameters.get("shape").optional().orElse("cube");
 
-        BlockArea area = selectionOwner.getSelection();
-        BlockPos p1 = area.getPoint1();
-        BlockPos p2 = area.getPoint2();
-
-        switch (shape)
+        runShape(sender, shape, pos ->
         {
-            case "cube":
-                for (BlockPos pos : area)
-                {
-                    IBlockState state = dst.get(world.rand().nextInt(dst.size()));
-                    world.setBlockState(pos, state, 2);
-                }
-                break;
-            case "sphere":
-            {
-                double[] spheroidOrigin = new double[]{(p1.getX() + p2.getX()) * 0.5, (p1.getY() + p2.getY()) * 0.5, (p1.getZ() + p2.getZ()) * 0.5};
-                int[] areaSize = area.areaSize();
-                double[] spheroidSize = new double[]{areaSize[0] * 0.5, areaSize[1] * 0.5, areaSize[2] * 0.5};
-
-                for (BlockPos coord : area)
-                {
-                    double[] coordPoint = new double[]{coord.getX(), coord.getY(), coord.getZ()};
-                    if (IvShapeHelper.isPointInSpheroid(coordPoint, spheroidOrigin, spheroidSize))
-                    {
-                        IBlockState state = dst.get(world.rand().nextInt(dst.size()));
-                        world.setBlockState(coord, state, 2);
-                    }
-                }
-                break;
-            }
-            default:
-                throw new WrongUsageException(getUsage(sender));
-        }
-
+            IBlockState state = dst.get(world.rand().nextInt(dst.size()));
+            world.setBlockState(pos, state, 2);
+        });
     }
 }
